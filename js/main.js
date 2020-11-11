22947203ec86dfd7b12192393c78cea384d5ff2a
Xuefei <xuefei.shi@mail.mcgill.ca>
2020-11-11 15:28:25 -0500

    svg animation

--- js/main.js
+++ js/main.js
@@ -5,3 +5,9 @@ $(function () {
 $('.button').click(()=>{
     $('#contact').toggleClass('expand');
 });
+
+// const name = document.querySelector('#name .start');
+// console.log(`letter  is ${name.getTotalLength()}`);
+// // for(let i = 0; i<name.length;i++){
+// //     console.log(`letter ${i+1} is ${name[i].getTotalLength()}`);
+// // }
